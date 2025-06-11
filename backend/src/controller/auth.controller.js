c0530f3aeb5784cb3146fbf4c57391e8f6b2c090
prachi1025 <prachipargal123@gmail.com>
2025-06-12 03:20:42 +0530

    make login and logout api

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -5,6 +5,10 @@ import { generateToken } from "../lib/utils.js"
 export const signup = async (req, res) => {
     const {fullName, email, password} = req.body
     try {
+        if (!fullName || !password || !email) {
+            return res.status(400).json({ message: "All fields are required"}) 
+        }
+
         if (password.length < 8) {
             return res.status(400).json({ message: "Password must be atleast 8 characters "})
         }
@@ -42,10 +46,41 @@ export const signup = async (req, res) => {
     }
 }
 
-export const login = (req, res) => {
-  
+export const login = async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const user = await User.findOne({ email })
+
+        if (!user) {
+            return res.status(400).json({ message: "Invalid credentials" })
+        }
+
+        const isPasswordCorrect = await bcrypt.compare(password, user.password)
+        if (!isPasswordCorrect) {
+            return res.status(400).json({ message: "Invalid credentials" })
+        }
+
+        generateToken(user._id, res)
+
+        res.status(200).json({
+            _id: user._id,
+            fullName: user.fullName,
+            email: user.email,
+            password: user.password,
+        })
+
+    } catch (error) {
+        console.log("error in login controller", error.message)
+        res.status(500).json({ message: "Internal server Error" })
+    }
 }
 
 export const logout = (req, res) => {
-    res.send("logout route")
+    try {
+        res.cookie("jwt", "", { maxAge: 0})
+        res.status(200).json({ message: "Logged out successfully"})
+    } catch (error) {
+        console.log("Error in the logout controller", error.message)
+        res.status(500).json({ message: "Internal server Error"})
+    }
 }
