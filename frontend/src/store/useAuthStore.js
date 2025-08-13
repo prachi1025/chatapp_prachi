ea3452a3241b78d7411a7a00c975a14f651c49ea
prachi1025 <prachipargal123@gmail.com>
2025-08-14 03:07:49 +0530

    Implement login functionality and UI improvements
    Added a login form with email and password fields, password visibility toggle, and loading state to LoginPage. Integrated login logic in useAuthStore with API call and toast notifications. Minor UI adjustment to SignUpPage layout.

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -48,5 +48,18 @@ export const useAuthStore = create((set) => ({
         } catch (error) {
             toast.error(error.response.data.message)
         }
+    },
+
+    login: async (data) => {
+        set({ isLoginIn: true })
+        try {
+            const res = await axiosInstance.post("/auth/login", data)
+            set({ authUser: res.data})
+            toast.success("Logged in successfully")
+        } catch (error) {
+            toast.error(error.response.data.message)
+        } finally {
+            set({ isLoginIn: false })
+        }
     }
 }))
